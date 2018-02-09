fc868a631207adc8cee9c3c63a63ec34cdd9273f
Hannah Bunce <hannahlbunce@gmail.com>
2018-02-09 20:01:33 +0000

    Add in token data for Hard and Expert

--- chaos.js
+++ chaos.js
@@ -47,3 +47,56 @@ window.randomTokenStandard = () => {
   const randomToken = tokenNameStandard[randomIndex]
   console.log(randomToken)
 }
+
+const tokenNameHard = [
+  "0",
+  "0",
+  "0",
+  "-1",
+  "-1",
+  "-2",
+  "-2",
+  "-3",
+  "-3",
+  "-4",
+  "-5",
+  "Peril - Refer to the scenario card for resolution.",
+  "Peril - Refer to the scenario card for resolution.",
+  "Cultist - Refer to the scenario card for resolution.",
+  "Eltdown Shards - Refer to the scenario card for resolution.",
+  "Tentacle - Instant fail.",
+  "Elder Sign - Refer to your character card for resolution."
+]
+
+window.randomTokenHard = () => {
+  const randomIndex = Math.floor(Math.random() * tokenNameHard.length)
+  const randomToken = tokenNameHard[randomIndex]
+  console.log(randomToken)
+}
+
+const tokenNameExpert = [
+  "0",
+  "-1",
+  "-1",
+  "-2",
+  "-2",
+  "-3",
+  "-3",
+  "-4",
+  "-4",
+  "-5",
+  "-6",
+  "-8",
+  "Peril - Refer to the scenario card for resolution.",
+  "Peril - Refer to the scenario card for resolution.",
+  "Cultist - Refer to the scenario card for resolution.",
+  "Eltdown Shards - Refer to the scenario card for resolution.",
+  "Tentacle - Instant fail.",
+  "Elder Sign - Refer to your character card for resolution."
+]
+
+window.randomTokenExpert = () => {
+  const randomIndex = Math.floor(Math.random() * tokenNameExpert.length)
+  const randomToken = tokenNameExpert[randomIndex]
+  console.log(randomToken)
+}
