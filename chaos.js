1f2eac5fff142550467897eed261d11a5f2fd70d
Hannah Bunce <hannahlbunce@gmail.com>
2018-02-09 19:52:15 +0000

    Add token data for Easy and Standard difficulty

--- chaos.js
+++ chaos.js
@@ -1,4 +1,29 @@
-const tokenName = [
+const tokenNameEasy = [
+  "+1",
+  "+1",
+  "0",
+  "0",
+  "0",
+  "-1",
+  "-1",
+  "-1",
+  "-2",
+  "-2",
+  "Peril - Refer to the scenario card for resolution.",
+  "Peril - Refer to the scenario card for resolution.",
+  "Cultist - Refer to the scenario card for resolution.",
+  "Eltdown Shards - Refer to the scenario card for resolution.",
+  "Tentacle - Instant fail.",
+  "Elder Sign - Refer to your character card for resolution."
+]
+
+window.randomTokenEasy = () => {
+  const randomIndex = Math.floor(Math.random() * tokenNameEasy.length)
+  const randomToken = tokenNameEasy[randomIndex]
+  console.log(randomToken)
+}
+
+const tokenNameStandard = [
   "+1",
   "0",
   "0",
@@ -17,12 +42,8 @@ const tokenName = [
   "Elder Sign - Refer to your character card for resolution."
 ]
 
-console.log('loading a')
-
-window.randomToken = () => {
-  const randomIndex = Math.floor(Math.random() * tokenName.length)
-  const randomToken = tokenName[randomIndex]
+window.randomTokenStandard = () => {
+  const randomIndex = Math.floor(Math.random() * tokenNameStandard.length)
+  const randomToken = tokenNameStandard[randomIndex]
   console.log(randomToken)
 }
-
-console.log('loading b')
