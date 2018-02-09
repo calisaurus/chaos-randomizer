2954d010a82e3dd5cf61a750b2060e3f00002645
Hannah Bunce <hannahlbunce@gmail.com>
2018-02-09 21:49:16 +0000

    Split token data into chaos-tokens.js

---
+++ chaos-tokens.js
@@ -0,0 +1,78 @@
+const tokenNameEasy = [
+  "+1",
+  "+1",
+  "0",
+  "0",
+  "0",
+  "-1",
+  "-1",
+  "-1",
+  "-2",
+  "-2",
+  "Peril - Refer to the scenario card for resolution.",
+  "Peril - Refer to the scenario card for resolution.",
+  "Cultist - Refer to the scenario card for resolution.",
+  "Eltdown Shards - Refer to the scenario card for resolution.",
+  "Tentacle - Instant fail.",
+  "Elder Sign - Refer to your character card for resolution."
+]
+
+const tokenNameStandard = [
+  "+1",
+  "0",
+  "0",
+  "-1",
+  "-1",
+  "-1",
+  "-2",
+  "-2",
+  "-3",
+  "-4",
+  "Peril - Refer to the scenario card for resolution.",
+  "Peril - Refer to the scenario card for resolution.",
+  "Cultist - Refer to the scenario card for resolution.",
+  "Eltdown Shards - Refer to the scenario card for resolution.",
+  "Tentacle - Instant fail.",
+  "Elder Sign - Refer to your character card for resolution."
+]
+
+const tokenNameHard = [
+  "0",
+  "0",
+  "0",
+  "-1",
+  "-1",
+  "-2",
+  "-2",
+  "-3",
+  "-3",
+  "-4",
+  "-5",
+  "Peril - Refer to the scenario card for resolution.",
+  "Peril - Refer to the scenario card for resolution.",
+  "Cultist - Refer to the scenario card for resolution.",
+  "Eltdown Shards - Refer to the scenario card for resolution.",
+  "Tentacle - Instant fail.",
+  "Elder Sign - Refer to your character card for resolution."
+]
+
+const tokenNameExpert = [
+  "0",
+  "-1",
+  "-1",
+  "-2",
+  "-2",
+  "-3",
+  "-3",
+  "-4",
+  "-4",
+  "-5",
+  "-6",
+  "-8",
+  "Peril - Refer to the scenario card for resolution.",
+  "Peril - Refer to the scenario card for resolution.",
+  "Cultist - Refer to the scenario card for resolution.",
+  "Eltdown Shards - Refer to the scenario card for resolution.",
+  "Tentacle - Instant fail.",
+  "Elder Sign - Refer to your character card for resolution."
+]
