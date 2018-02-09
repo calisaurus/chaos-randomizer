7b982af66bbfaa8ee8b5b574cb3bea420318703f
Hannah Bunce <hannahlbunce@gmail.com>
2018-02-09 21:41:48 +0000

    Display random result to webpage for all difficulties

--- chaos.js
+++ chaos.js
@@ -21,6 +21,11 @@ window.randomTokenEasy = () => {
   const randomIndex = Math.floor(Math.random() * tokenNameEasy.length)
   const randomToken = tokenNameEasy[randomIndex]
   console.log(randomToken)
+  return randomToken;
+}
+
+displayEasy = (a) => {
+  document.getElementById("demo").innerHTML = a;
 }
 
 const tokenNameStandard = [
@@ -46,6 +51,11 @@ window.randomTokenStandard = () => {
   const randomIndex = Math.floor(Math.random() * tokenNameStandard.length)
   const randomToken = tokenNameStandard[randomIndex]
   console.log(randomToken)
+  return randomToken;
+}
+
+displayStandard = (a) => {
+  document.getElementById("demo").innerHTML = a;
 }
 
 const tokenNameHard = [
@@ -72,6 +82,11 @@ window.randomTokenHard = () => {
   const randomIndex = Math.floor(Math.random() * tokenNameHard.length)
   const randomToken = tokenNameHard[randomIndex]
   console.log(randomToken)
+  return randomToken;
+}
+
+displayHard = (a) => {
+  document.getElementById("demo").innerHTML = a;
 }
 
 const tokenNameExpert = [
@@ -99,4 +114,9 @@ window.randomTokenExpert = () => {
   const randomIndex = Math.floor(Math.random() * tokenNameExpert.length)
   const randomToken = tokenNameExpert[randomIndex]
   console.log(randomToken)
+  return randomToken;
+}
+
+displayExpert = (a) => {
+  document.getElementById("demo").innerHTML = a;
 }
